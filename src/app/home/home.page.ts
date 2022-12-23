5d2076864b5acc8dc3e3104d61d273b5b2e1a637
ionic-solutions <ionicsolutions92@gmail.com>
2022-12-22 20:37:21 -0800

    add all files

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,110 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import  firebase  from 'firebase/compat/app'
+import { AuthserviceService } from '../authservice.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  CountryJson = environment.CountryJson;
+  OTP: string = '';
+  Code: any;
+  PhoneNo: any;
+  CountryCode: any = '+91';
+  showOTPInput: boolean = false;
+  OTPmessage: string = 'An OTP is sent to your number. You should receive it in 15 s'
+  recaptchaVerifier: firebase.auth.RecaptchaVerifier;
+  confirmationResult: any;
+
+  constructor(
+    private alertController: AlertController,
+    private authService: AuthserviceService,
+    public router:Router
+  ) {}
+
+  async ionViewDidEnter() {
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      size: 'invisible',
+      callback: (response) => {
+
+      },
+      'expired-callback': () => {
+      }
+    });
+  }
+  ionViewDidLoad() {
+    this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+      size: 'invisible',
+      callback: (response) => {
+
+      },
+      'expired-callback': () => {
+      }
+    });
+  }
+
+  countryCodeChange($event) {
+    this.CountryCode = $event.detail.value;
+  }
+  // Button event after the nmber is entered and button is clicked
+  signinWithPhoneNumber($event) {
+    console.log('country', this.recaptchaVerifier);
+
+    if (this.PhoneNo && this.CountryCode) {
+      this.authService.signInWithPhoneNumber(this.recaptchaVerifier, this.CountryCode + this.PhoneNo).then(
+        success => {
+          this.OtpVerification();
+        }
+      );
+    }
+  }
+
+  async showSuccess() {
+    const alert = await this.alertController.create({
+      header: 'Success',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: (res) => {
+            alert.dismiss();
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+  async OtpVerification() {
+    const alert = await this.alertController.create({
+      header: 'Enter OTP',
+      backdropDismiss: false,
+      inputs: [
+        {
+          name: 'otp',
+          type: 'text',
+          placeholder: 'Enter your otp',
+        }
+      ],
+      buttons: [{
+        text: 'Enter',
+        handler: (res) => {
+          this.authService.enterVerificationCode(res.otp).then(
+            userData => {
+              this.showSuccess();
+              this.router.navigate(['welcome']);
+              console.log(userData);
+            }
+          );
+        }
+      }
+      ]
+    });
+    await alert.present();
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,39 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  },
+  CountryJson: [
+    { name: 'Pakistan', dial_code: '+92', code: 'PK' },
+    { name: 'Afghanistan', dial_code: '+93', code: 'AF' },
+    { name: 'Albania', dial_code: '+35', code: 'AL' },
+    { name: 'Algeria', dial_code: '+213', code: 'DZ' },
+    { name: 'AmericanSamoa', dial_code: '+1 684', code: 'AS' },
+    { name: 'Andorra', dial_code: '+376', code: 'AD' },
+    { name: 'Angola', dial_code: '+244', code: 'AO' },
+    { name: 'Anguilla', dial_code: '+1 264', code: 'AI' },
+    { name: 'Antigua and Barbuda', dial_code: '+1268', code: 'AG' },
+    { name: 'Argentina', dial_code: '+54', code: 'AR' },
+    { name: 'Armenia', dial_code: '+374', code: 'AM' },
+    { name: 'Aruba', dial_code: '+297', code: 'AW' },
+    ]
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
